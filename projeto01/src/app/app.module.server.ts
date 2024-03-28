99d78140a28aeb4d5edb072a6fde2ae081c21fc6
William Felipe <williamfelipe081@gmail.com>
2024-03-27 23:02:25 -0300

    Finalizacao da aula-03 - entendendo mais e criando modulos

--- projeto01/src/app/app.module.server.ts
+++ projeto01/src/app/app.module.server.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [AppModule, ServerModule],
+  imports: [ServerModule],
   bootstrap: [AppComponent],
 })
 export class AppServerModule {}
--- projeto01/src/app/app.module.ts
+++ projeto01/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { PrimeiroComponent2 } from './meu-primeiro-component2/meu-primeiro-component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [AppComponent, MeuPrimeiroComponent, PrimeiroComponent2],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideClientHydration()],
+  imports: [BrowserModule, AppRoutingModule, CursosModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
