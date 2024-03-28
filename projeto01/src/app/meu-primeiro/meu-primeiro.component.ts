a7671a3e8367bb7b6248f2dda703c08486cbea87
William Felipe <williamfelipe081@gmail.com>
2024-03-28 15:33:47 -0300

    Finalizacao da aula 04 - templates, interpolacao e diretivas

--- projeto01/src/app/meu-primeiro/meu-primeiro.component.ts
+++ projeto01/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-meu-primeiro',
   templateUrl: './meu-primeiro.component.html',
-  styleUrl: './meu-primeiro.component.css'
+  styleUrl: './meu-primeiro.component.css',
 })
 export class MeuPrimeiroComponent {
+  cursos: string[];
 
+  constructor() {
+    this.cursos = ['Java', 'Angular', 'Javascript'];
+  }
 }
